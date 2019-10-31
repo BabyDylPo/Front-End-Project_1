9971c3a604fc7ed05024c21aa7d8b919ed85f61f
dylpo <matthewsdb34@gmail.com>
2019-10-31 00:32:29 -0700

    near finish todo list check todo js to keep working

--- src/todo_list.js
+++ src/todo_list.js
@@ -1,7 +1,9 @@
-const todos = [];
+console.log(window.localStorage);
+const todos = JSON.parse(localStorage.getItem('todos')) || [];
 
 const $todos = $(".todos");
 const $addTodoForm = $(".add-todo-form");
+
 
 addTodo = () => {
     let val = $addTodoForm.find("input[type=text]").val();
@@ -14,19 +16,56 @@ addTodo = () => {
     todos.push(todo);
     
     populateList();
+    locallyStoreTodos();
 }
 
 populateList = () => {
-    todos.map(( todo ) => {
-        let val = todo.value;
-        let dn =todo.done;
-        todo.value = $(`<label>${val}</label>`);
-        todo.done = $(`<input type="checkbox" value="${dn}">`);
+    //reset $todos children so duplicates arent added
+    let children = Array.from($todos.children())
+    // If Yes, then DESTROY THEM ALL
+    if (children) {
+        children.forEach((child) => {
+            child.remove();
+        })
+    }
+
+    // iterate through todos and append a $todo to $todos
+    todos.forEach(( todo ) => {
+        let $todo;
+
+        $todo = $(`<li><input type="checkbox" value="${todo.done} class="check-box"><label>${todo.value}</label></li>`);
+        $todos.append($todo);
     })
-    console.log(todos);
+
+
+    // WORKING HERE LAST THING WRITTEN
+    checkBoxes();
+    
 }
+
+locallyStoreTodos = () => {
+    // can only store strings on localStorage
+    window.localStorage.setItem('todos', JSON.stringify(todos));
+    console.log(window.localStorage);
+}
+
+checkBoxes = () => {
+    //create a $checkBoxes variable so I can iterate through all inputs and get value from them.
+    if (todos) {
+        const $checkBoxes = $(".todos :input");
+        console.log($checkBoxes);
+    }
+    //Not finished
+}
+
+populateList();
+
 
 $addTodoForm.submit( (event) => {
     event.preventDefault();
     addTodo();
 });
+
+
+
+
